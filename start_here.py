192e00bf953b41809b38acb5365874aaa758a7c7
Andrew <andrewtheis4@gmail.com>
2022-03-14 21:28:09 -0700

    hello world

---
+++ start_here.py
@@ -0,0 +1,31 @@
+print("hello world!")
+
+# Welcome to Python programming on twitch!
+# Run this program and you'll be a computer programmer
+
+# this is a comment. Ptyhon won't run this code.
+"""
+This is a multi-line-comment.
+The computer still won't run this and its used to talk about more complicated
+concepts in code. :)
+"""
+
+# Primitive Types
+6  # integer
+6.6  # float
+'butts'  # string
+True  # boolean
+
+# variables
+sixty_six = 66
+meme_number = 6.9
+cat_names = 'MO & Marie'
+is_this_informative = True
+
+# output...
+print()  # built in function
+print(sixty_six)
+print(meme_number)
+print("We love", cat_names)
+print("This is informative?", is_this_informative)
+
